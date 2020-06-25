85ef60bbadda83072b5b9a8e24c8068dbcff1533
VICTOR GERARDO <victorgerardo@MacBook-Pro-de-Victor.local>
2020-06-25 12:22:26 -0500

    Updates

---
+++ precache-manifest.b2562dd12e2ded6f564bbaddbd3b1094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983f38e1cf2e29c4444a2e342dd78ddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d9f2d3fdbed18b5a9a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9962ff181d5fcd2fb0c2",
+    "url": "/static/js/2.faf47abb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.faf47abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d9f2d3fdbed18b5a9a",
+    "url": "/static/js/main.248377a9.chunk.js"
+  },
+  {
+    "revision": "d43fa0c241226640ede2",
+    "url": "/static/js/runtime-main.03090214.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
